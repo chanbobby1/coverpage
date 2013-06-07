3fbb8be0c768ebd7c1073f9eeabe4947d8399b36
Bobby Chan <chan.bobby1@gmail.com>
2013-06-07 02:37:04 -0400

    Hide speech modal

--- index.php
+++ index.php
@@ -74,20 +74,12 @@
 			</li>
 		</ul>
 	
-		<div class="speech-modal">
+		<!--<div class="speech-modal">
 			<div class="speech-wrapper">
 		      <input type="speech" id="speech" x-webkit-speech />
 		    </div>
 		    <!--<div class="answer"></div>-->
-		</div>
-	
-		<!--<div class="slantartwork">
-			<div class="slant1"></div>
-			<div class="slant2"></div>
-			<div class="slant3"></div>
-		</div>-->
-		
-		
+		<!--</div>-->
 		
 		<!-- CTV News Video feed -->
 		<!--<div class="videofeed">
@@ -109,5 +101,18 @@
 	<script>
 		$(document).ready(function() {
 		});
+	</script>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-23783720-5']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	
 	</script>
 </html>
